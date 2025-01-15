0bf60d3f201eb92f7408876eff87141d462af112
ashraful2871 <ashrafulislam2758@gmail.com>
2025-01-15 17:55:48 +0600

    update registration fee with patch request

--- index.js
+++ index.js
@@ -166,6 +166,24 @@ async function run() {
       res.send(result);
     });
 
+    //add session fee
+    app.patch("/approve-session/:id", verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const { amount, status } = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          registrationFee: amount,
+          status: status,
+        },
+      };
+      const result = await createStudySessionCollection.updateOne(
+        filter,
+        updatedDoc
+      );
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
