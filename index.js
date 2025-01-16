e8ebde826df1f2d89d3175be08ac706d5a7f13d7
ashraful2871 <ashrafulislam2758@gmail.com>
2025-01-16 11:05:11 +0600

    delete specific note dtaa

--- index.js
+++ index.js
@@ -231,6 +231,14 @@ async function run() {
       res.send(result);
     });
 
+    //delete note
+    app.delete("/note/:id", verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await noteCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
