a75b02d5593cdae73978d9f7cad006c9d9ec8320
ashraful2871 <ashrafulislam2758@gmail.com>
2025-01-18 20:35:47 +0600

    remove this comment // Connect the client to the server	(optional starting in v4.7)     // await client.connect();

--- index.js
+++ index.js
@@ -23,9 +23,6 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-
     const db = client.db("StudyDB");
     const createStudySessionCollection = db.collection("create-study-session");
     const materialCollection = db.collection("material");
